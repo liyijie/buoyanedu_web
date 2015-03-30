4c15967c34514f52ff5e48d286815a95303447f0
liyijie <liyijie825@gmail.com>
2015-03-30 13:04:33 +0800

    add course scaffold

---
+++ app/models/course.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  desc       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Course < ActiveRecord::Base
+end
